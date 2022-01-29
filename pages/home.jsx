e4a099a784507024101e0e27581d51f28444cf45
Rob Magee <robmagee@gmail.com>
2022-01-29 12:19:54 -0700

    Initial

---
+++ pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Paper} from "@material-ui/core/Paper";
+
+Home.propTypes = {
+
+};
+
+function Home(props) {
+    return (
+        <Paper></Paper>
+    );
+}
+
+export default Home;
